ef5077b741f932418b5fe0bea4a929f5b38d5705
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 11:04:19 +0200

    Update settings.js for M-PI

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;uZcARBbL#Y6l9_Hpm71ClNDmdCNvClJUPpigRE6Us1UxNztbGDF8",
+  OWNER_NUMBER: "+254745875869", 
+  PREFIX: "!"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
