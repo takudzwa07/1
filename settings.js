d5716c1282afb69890ea698e656e7657d2415834
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 10:58:38 +0200

    Update settings.js for Pinchez

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;vd0hyAqQ#9kfpJOOZ2b54iyk_4gS6lxtyfQ3givXw8cfa7pq3EeQ",
+  OWNER_NUMBER: "+254745875869", 
+  PREFIX: "!"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
