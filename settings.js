936537501b69bf2cf8459229b523d76b83429d30
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-13 16:13:00 +0200

    Update settings.js for Purefibre-Bot

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;HJ0S0KxS#bjgRwOiarXHTsr-JhadiiUzUxU7moIjsFB5fHgyKx6c",
+  OWNER_NUMBER: "+6285713806067", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
